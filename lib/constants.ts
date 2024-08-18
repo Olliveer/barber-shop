d4e05451c8071df43353079334e2a9d283fd49f3
Olliveer <j.oliveira90@live.com>
2024-08-18 19:18:44 -0300

    feat: add  sheet component for menu

---
+++ lib/constants.ts
@@ -0,0 +1,31 @@
+interface QuickSearchOption {
+  imageUrl: string;
+  title: string;
+}
+
+export const quickSearchOptions: QuickSearchOption[] = [
+  {
+    imageUrl: "/next.svg",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "/next.svg",
+    title: "Barba",
+  },
+  {
+    imageUrl: "/next.svg",
+    title: "Acabamento",
+  },
+  {
+    imageUrl: "/next.svg",
+    title: "Massagem",
+  },
+  {
+    imageUrl: "/next.svg",
+    title: "Sobrancelha",
+  },
+  {
+    imageUrl: "/next.svg",
+    title: "Hidratação",
+  },
+];
